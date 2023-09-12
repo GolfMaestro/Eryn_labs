4317ee92ef3d076b98a6e72fd92f8e3223298871
Vlad Eryn <vlad2004e@gmail.com>
2023-09-12 22:15:08 +0300

    Added menu

--- Eryn_labs/Eryn_labs.cpp
+++ Eryn_labs/Eryn_labs.cpp
@@ -1,20 +1,73 @@
-// Eryn_labs.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
+
+void menu(int choose);  // declare the functions
+void print_menu();
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int action;
+    print_menu();
+
+    for (;;) {  // eternal loop
+        cin >> action;
+        menu(action);
+
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void menu(int choose) {
+
+    if (choose == 1) {
+        cout << "Add pipe\n";
+        cout << "0. Return to menu\n";
+    }
+
+    if (choose == 2) {
+        cout << "Add compressor station\n";
+        cout << "0. Return to menu\n";
+    }
+
+    if (choose == 3) {
+        cout << "View all objects\n";
+        cout << "0. Return to menu\n";
+    }
+
+    if (choose == 4) {
+        cout << "Edit pipe\n";
+        cout << "0. Return to menu\n";
+    }
+
+    if (choose == 5) {
+        cout << "Edit compressor station\n";
+        cout << "0. Return to menu\n";
+    }
+
+    if (choose == 6) {
+        cout << "Save\n";
+        cout << "0. Return to menu\n";
+    }
+
+    if (choose == 7) {
+        cout << "Load\n";
+        cout << "0. Return to menu\n";
+    }
+
+    if (choose == 0) {
+        print_menu();
+    }
+
+}
+
+void print_menu() {
+    cout << "1. Add pipe\n";
+    cout << "2. Add compressor station\n";
+    cout << "3. View all objects\n";
+    cout << "4. Edit pipe\n";
+    cout << "5. Edit compressor station\n";
+    cout << "6. Save\n";
+    cout << "7. Load\n";
+    cout << "0. Return to menu\n";
+}
